e77fc0165dae8cf79367fb1491acf1f0d2159b29
krititwa <krititwaray97@gmail.com>
2026-03-29 11:39:20 +0530

    set basic setup

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import "dotenv/config"
+import express from "express";
+const app = express();
+
+
+app.use(express.json({ limit: '150mb' }));
+
+
+
+
+
+const PORT = process.env.PORT; // 3000;
+app.listen(PORT,() => {
+    console.log(`Server is listening on port ${PORT}`)
+})
